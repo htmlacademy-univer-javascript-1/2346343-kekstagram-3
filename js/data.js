c4628087c35ba46d9a3750a86bc3ca7f44d45ea5
Zmiha <ivan.drugoff@yandex.ru>
2023-04-20 05:05:46 +0300

    modulation

---
+++ js/data.js
@@ -0,0 +1,20 @@
+import { getRandom } from "./util";
+
+const photoData = [];
+
+const createUser = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: `photo number ${id}`,
+  likes: getRandom(15, 200),
+  comment: getRandom(0, 200)
+});
+
+export function getPhotoData() {
+  for (let i = 1; i <= 25; i++) {
+    photoData.push(createUser(i));
+  }
+  return photoData;
+}
+
+getPhotoData();
