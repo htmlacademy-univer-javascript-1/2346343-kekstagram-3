0ba08d029bd6421769eb5a6bbd94ddcb488fa07e
Zmiha <ivan.drugoff@yandex.ru>
2023-05-08 05:45:17 +0300

    module7

---
+++ js/render.js
@@ -0,0 +1,20 @@
+import {getPhotoData} from './data.js';
+
+let photoData = getPhotoData();
+
+const userTemp = document.querySelector('#picture').content.querySelector('.picture');
+const pictures = document.querySelector('.pictures');
+const patternUserFragment = document.createDocumentFragment();
+
+export const render = () => {
+    for (let i = 0; i < photoData.length; i++ ) {
+        let user = photoData[i];
+
+        const temp = userTemp.cloneNode(true);
+        temp.querySelector('.picture__img').setAttribute('src', user.url);
+        temp.querySelector('.picture__info').querySelector('.picture__comments').textContent = user.comment;
+        temp.querySelector('.picture__info').querySelector('.picture__likes').textContent = user.likes;
+        patternUserFragment.append(temp);
+    }
+    pictures.append(patternUserFragment);
+}
